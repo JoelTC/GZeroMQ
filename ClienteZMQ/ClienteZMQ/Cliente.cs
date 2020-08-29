81ca64ea26f43f5e8e47c516123adbd201ae624c
JoelTC <57535643+JoelTC@users.noreply.github.com>
2020-08-29 11:08:09 -0500

    ZeroMQ

---
+++ ClienteZMQ/ClienteZMQ/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClienteZMQ
+{
+    static class Servidor
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Connecting to hello world server…");
+            using (var requester = new RequestSocket())
+            {
+                requester.Connect("tcp://localhost:5555");
+                Console.WriteLine("Se conectó");
+
+                Console.WriteLine("Sending Hello");
+                requester.SendFrame("Hello");
+                string str = requester.ReceiveFrameString();
+                Console.WriteLine(str);
+                //Console.WriteLine("Received World {0}", requestNumber);
+
+            }
+        }
+    }
+}
---
+++ ServidorZMQ/ServidorZMQ/Json.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorZMQ
+{
+    public class Json
+    {
+        public int id_producto { get; set; }
+        public int id_cliente { get; set; }
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+        public int diferencia { get; set; }
+    }
+}
